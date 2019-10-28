f2e52e8c3afeb57bdcdfd982d8440ec2e1bc7c84
Claudia Friedrich <claudae_f@hotmail.com>
2019-10-28 13:23:34 +0100

    404

--- template-parts/content/content-404.php
+++ template-parts/content/content-404.php
@@ -1,21 +1,7 @@
 <div class="site-404">
-
-  <h1 class="site-404__title"><?php _e( '404-title', 'theme-neidhartschoen' ); ?></h1>
-
-  <p class="site-404__text"><?php _e( '404-not-found-text', 'theme-neidhartschoen' ); ?></p>
-
-  <a href="<?= home_url('/') ?>" class="site-404__back-link nhs-btn nhs-arrow-btn">
-    <span class="nhs-arrow-btn__arrow nhs-arrow-btn__arrow--left">
-      <span class="nhs-arrow-btn__shaft"></span>
-    </span>
-    <span class="nhs-arrow-btn__main">
-      <span class="nhs-arrow-btn__text"><?php _e( '404-back-to-startpage', 'theme-neidhartschoen' ); ?></span>
-      <span class="nhs-arrow-btn__arrow nhs-arrow-btn__arrow--right">
-        <span class="nhs-arrow-btn__shaft"></span>
-      </span>
-    </span>
-  </a>
-
-
+  <p class="title-h1">Hello… is it <button class="title-h1__button">me</button> you’re looking 404</p>
+  <div class="video active">
+    <iframe class="video__item" id="playVideo" width="560" height="315" src="https://www.youtube.com/embed/AiC7ZX5K9L4?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
 </div>
 
