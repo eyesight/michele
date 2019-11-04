ea54f33e9b9270268d0ffac9544900d0cb30c08e
Claudia Friedrich <claudae_f@hotmail.com>
2019-11-04 23:27:03 +0100

    correction in prevNext

--- template-parts/content/content-prevNext.php
+++ template-parts/content/content-prevNext.php
@@ -1,6 +1,6 @@
 <div class="content prevNext">
     <?php
-        previous_post_link('<div class="prevNext__arrow prevNext__left"><span>%link</span></div>');
-        next_post_link('<div class="prevNext__arrow prevNext__right"><span>%link</span></div>');
+        next_post_link('<div class="prevNext__arrow prevNext__left"><span>%link</span></div>');
+        previous_post_link('<div class="prevNext__arrow prevNext__right"><span>%link</span></div>');
     ?>  
 </div>  
