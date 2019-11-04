b9cf886b913b02afc7233ff9d4d19a195e1925ab
Claudia Friedrich <claudae_f@hotmail.com>
2019-11-04 23:22:57 +0100

    prevNext

--- template-parts/content/content-prevNext.php
+++ template-parts/content/content-prevNext.php
@@ -1,6 +1,6 @@
-<div>
+<div class="content prevNext">
     <?php
-        previous_post_link('<div class="left">&laquo; %link</div>');
-        next_post_link('<div class="right">%link &raquo;</div>');
+        previous_post_link('<div class="prevNext__arrow prevNext__left"><span>%link</span></div>');
+        next_post_link('<div class="prevNext__arrow prevNext__right"><span>%link</span></div>');
     ?>  
 </div>  
