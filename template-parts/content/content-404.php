889f11787c8d1e3047cdf9e18f0f427543149a1b
Claudia Friedrich <claudae_f@hotmail.com>
2020-02-02 23:14:03 +0100

    corrections in 404 and new template

--- template-parts/content/content-404.php
+++ template-parts/content/content-404.php
@@ -1,7 +1,7 @@
 <div class="site-404">
-  <p class="title-h1">Hello… is it <button class="title-h1__button">me</button> you’re looking 404</p>
-  <div class="video active">
-    <iframe class="video__item" id="playVideo" width="560" height="315" src="https://www.youtube.com/embed/AiC7ZX5K9L4?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  </div>
-</div>
+  <p class="title-h1">Hello… is it 
+  <button class="title-h1__button">me
+  <img class="video" src="<?php echo get_template_directory_uri(); ?>/dist/img/GIF_404.gif" />
+  </button> you’re looking 404</p>
+</div>  
 
