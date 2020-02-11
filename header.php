ad99ca0e121c2c389aad6d1a707f6f9b7c7a2038
Claudia Friedrich <claudae_f@hotmail.com>
2020-02-11 23:06:19 +0100

    added google analytics and new fonts

--- header.php
+++ header.php
@@ -3,6 +3,14 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src='https://www.googletagmanager.com/gtag/js?id=UA-158008354-1'></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-158008354-1');
+    </script>
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
