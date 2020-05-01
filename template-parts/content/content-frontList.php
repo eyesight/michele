6dd3cdd664f3cd94776614668c653f076fc73f22
Claudia Friedrich <claudae_f@hotmail.com>
2020-05-01 23:47:00 +0200

    some fixes

--- template-parts/content/content-frontList.php
+++ template-parts/content/content-frontList.php
@@ -1,7 +1,7 @@
 <section class="layout-bg">
-        <div class="layout-bg__inner" data-sticky-container>
+        <div class="layout-bg__inner" data-sticky-container >
             <div class="scroll-element">
-                <a href="<?php echo get_category_link(get_field('category-information-front'));?>" data-sticky-class="scroll-element__link--sticky" class="scroll-element__link"> 
+                <a href="<?php echo get_category_link(get_field('category-information-front'));?>" data-sticky-class="scroll-element__link--sticky" class="scroll-element__link" data-margin-top=125> 
                     <h3 class="scroll-element__title"><?php the_field('list-reasons-title');?></h3>
                     <button class="scroll-element__button">
                         <span class="scroll-element__button-text">Warum ich fliege</span><span class="scroll-element__button-arrow"></span>
