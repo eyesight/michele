4fc16c90b6d541db0a627719cafce5cb083fcc4e
Claudia Friedrich <claudae_f@hotmail.com>
2020-05-23 20:31:25 +0200

    added text-pages

--- pageWithHeroImage.php
+++ pageWithHeroImage.php
@@ -1,11 +1,11 @@
+<?php /* Template Name: Seite mit grossem Auftakt-Bild (Hero image) */ ?>
+
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) : ?>
 		<?php the_post(); ?>
-		<div class="title-h1">
-			<h1 class="title-h1__text"><?php the_title()?></h1>
-		</div>
+		<?php get_template_part( 'template-parts/content/content-hero' ); ?>
 		<?php get_template_part( 'template-parts/content/content' ); ?>
 	<?php endwhile; ?>
 <?php else : ?>
