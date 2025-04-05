8260db124385de93f042edc107aa31bff3a08ae7
Claudia Friedrich <claudae_f@hotmail.com>
2025-04-05 22:56:53 +0200

    404

--- template-parts/content/content-404.php
+++ template-parts/content/content-404.php
@@ -1,7 +1,9 @@
-<div class="site-404">
-  <p class="title-h1">Hello… is it 
-  <button class="title-h1__button">me
-  <img class="video" src="<?php echo get_template_directory_uri(); ?>/dist/img/GIF_404.gif" />
-  </button> you’re looking 404</p>
-</div>  
+<main class="content">
+  <div class="site-404">
+    <p class="title-h1">Hello… is it 
+    <button class="title-h1__button">me
+    <img class="video" src="<?php echo get_template_directory_uri(); ?>/dist/img/GIF_404.gif" />
+    </button> you’re looking 404</p>
+  </div>  
+</main>
 
